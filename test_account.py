1d864f8da7d2730815a9ef53c7210de280baab2a
Ben Kidder <r.ben.kidder@gmail.com>
2023-04-21 18:02:31 -0500

    Add tests for Account class methods init, deposit, and withdrawal

--- test_account.py
+++ test_account.py
@@ -1,2 +1,42 @@
 from account import Account
 
+
+class Test_Account:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        # Test Account class initial state
+        assert self.a1.get_name() == 'John'
+        assert self.a1.get_balance() == 0
+
+    def test_deposit(self):
+        # Test deposit method of Account class
+        assert self.a1.deposit(-1) is False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(1) is True
+        assert self.a1.get_balance() == 1
+        assert self.a1.deposit(0.1) is True
+        assert self.a1.get_balance() == 1.1
+
+    def test_withdrawal(self):
+        # Test withdrawal method of Account class
+        self.a1.deposit(100)
+
+        assert self.a1.withdrawal(-1) is False
+        assert self.a1.get_balance() == 100
+        assert self.a1.withdrawal(0) is False
+        assert self.a1.get_balance() == 100
+        assert self.a1.withdrawal(100.1) is False
+        assert self.a1.get_balance() == 100
+        assert self.a1.withdrawal(1) is True
+        assert self.a1.get_balance() == 99
+        assert self.a1.withdrawal(0.1) is True
+        assert self.a1.get_balance() == 98.9
+        assert self.a1.withdrawal(98.9) is True
+        assert self.a1.get_balance() == 0
